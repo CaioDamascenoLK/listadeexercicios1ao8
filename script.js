b1e74dc84af5c2a92921353e5e1c9cbb84895f75
lklordsjoga <lklordsjoga@gmail.com>
2025-05-05 13:41:55 +0000

    exemplos do marcio

---
+++ script.js
@@ -0,0 +1,93 @@
+//Exemplo 1
+/*function mostrarAlerta() {
+    let nome = "Marcio"
+    alert("Seja bem-vindo(a), " + nome)
+}
+
+mostrarAlerta()
+
+
+//Exemplo 2
+function somar(valorA, valorB) {
+    alert(valorA+valorB)
+}
+
+somar(20,12)
+
+
+//Exemplo 3
+function mensagemDeBoasVindas(nome, cargo) {
+    return `seja bem-vindo(a) ${cargo} ${nome}`
+}
+
+let nome = prompt("Digite o nome do funcionário:")
+let cargo = prompt("Digite o cargo do funcionário:")
+
+alert(mensagemDeBoasVindas(nome, cargo))
+console.log(mensagemDeBoasVindas(nome, cargo))
+
+
+//Exemplo 4
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2
+}
+
+let multiplicacao1 = multiplicacao(6,7)
+let multiplicacao2 = multiplicacao(9,10)
+let multiplicacao3 = multiplicacao(12,25)
+
+console.log(multiplicacao1)
+console.log(multiplicacao2)
+console.log(multiplicacao3)
+
+
+//Exemplo 5
+function parOuImpar(numero) {
+    if (numero % 2 === 0) {
+        return "PAR"
+    }
+
+    return "IMPAR"
+}
+
+console.log(parOuImpar(14))
+*/
+
+//Exemplo 6
+//função anônima antiga
+/*
+let numero = 10
+
+const button = document.querySelector("button")
+
+button.addEventListener("click", function() {
+    numero ++
+    console.log(numero)
+})
+
+console.log(numero)
+*/
+
+
+/*
+//Exemplo 7
+let numero = 10
+
+const button = document.querySelector("button")
+
+button.addEventListener("click", () => {
+    numero ++
+    console.log(numero)
+})
+
+console.log(numero)
+*/
+
+
+/*
+//Exemplo 8
+let somar = (valor1, valor2) => valor1 + valor2
+let multiplicacao = (numero1, numero2) => numero1 * numero2
+
+console.log(somar(10, 70))
+*/
